d51b95c4f1f8b65fc6d5909767b3b4021564f8a2
Jonathan Riordan <c13432152@mydit.ie>
2015-09-29 15:49:22 +0100

    Question 2

--- question1_week2.py
+++ question1_week2.py
@@ -1,6 +1,6 @@
 ans = 0
 
-for i in xrange(1, 10):
+for i in xrange(1, 1000):
     if i%3 == 0:
         ans += i
     elif i%5 == 0:
---
+++ question2_week2.py
@@ -0,0 +1,16 @@
+num1 = 0
+num2 = 1
+sum = 0
+var = 0
+
+while(sum < 4000000) :
+	sum = num1 + num2
+	if(sum % 2 == 0) :
+		var += sum
+		
+	num1 = num2
+	num2 = sum
+	print sum
+	
+
+print var
