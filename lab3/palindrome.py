eac22522f937f70052abd94b6f9b348db41ac6c9
Jonathan Riordan <c13432152@mydit.ie>
2015-09-30 15:58:19 +0100

    comments

--- lab3/palindrome.py
+++ lab3/palindrome.py
@@ -8,20 +8,20 @@ word = raw_input("Enter in a word\n")
 x = 0
 
 
-for x in range (len(word) / 2):
+for i in range (len(word) / 2):
 	#compare the first letter to the last letter in the word
 	if(word[x]) == (word[len(word)-x-1]):
 		x += 1
 	else:
-		t = False
-	
+		bool = False
+	#
 	if x == (len(word)/2):
-	  t = True
+	  bool = True
 	if x != (len(word)/2):
-	  t = False
+	  bool = False
 	
-#
-if t == True:
+# print statements depending on the boolean
+if bool == True:
 	print "True"
 else:
 	print "False"
