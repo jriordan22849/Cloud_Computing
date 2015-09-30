e1a2aebe840dc1aecb12e07ab2c815bf233f7c78
Jonathan Riordan <c13432152@mydit.ie>
2015-09-30 15:28:34 +0100

    another push

--- lab3/palindrome.py
+++ lab3/palindrome.py
@@ -7,7 +7,13 @@ for i in range (len(word) / 2):
 	if(word[x]) == (word[len(word)-x-1]):
 		x += 1
 	if x == (len(word)/2):
-	  print "True"
-	else:
-		print "False"
+	  t = True
+	if x != (len(word)/2):
+	  t = False
+	
+if t == True:
+	print "True"
+else:
+	print "False"
+	
 
