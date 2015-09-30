40e7596fa1092e49f26dfc8de7bd7d06a3b3ce3d
Jonathan Riordan <c13432152@mydit.ie>
2015-09-30 15:54:19 +0100

    x

--- lab3/palindrome.py
+++ lab3/palindrome.py
@@ -2,19 +2,25 @@
 #	C13432152
 #	Cloud computing
 
+# The scanf, user types in word
 word = raw_input("Enter in a word\n")
 
 x = 0
 
 
-for i in range (len(word) / 2):
+for x in range (len(word) / 2):
+	#compare the first letter to the last letter in the word
 	if(word[x]) == (word[len(word)-x-1]):
 		x += 1
+	else:
+		t = False
+	
 	if x == (len(word)/2):
 	  t = True
 	if x != (len(word)/2):
 	  t = False
 	
+#
 if t == True:
 	print "True"
 else:
