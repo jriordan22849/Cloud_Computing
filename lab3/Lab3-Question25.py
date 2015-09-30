4df03eaf8d8902c342f7b81a6aca3cdf6a35ccab
Jonathan Riordan <c13432152@mydit.ie>
2015-09-30 16:26:37 +0100

    fixed question 25

--- lab3/Lab3-Question25.py
+++ lab3/Lab3-Question25.py
@@ -7,26 +7,23 @@
 num1 = 0
 num2 = 1
 sum = num2
-var = 0
 counter = 0	# this is the index, it counts each time a new number is added to the Fibonacci sequence
 bool = True	#
+var = ""
 
 # while loop for Fibonacci sequence where the total is less than 4 million
-while(sum < 4000000) : 
+while(len(var) < 1000) : 
 	sum = num1 + num2
-	print sum
+	var = str(sum)
 	
 	#Counter increases after a new number is added to the sequence.
-	counter += 1
-	
+
 	#When the sum is equal to or greater and the boolean variable is true, this if statement will print out the index term obce and then the boolean is set to false.
-	if sum >= 1000 and bool == True:
-		print "Index of the first term in the Fibonacci sequence to contain 1000 digits is", counter + 1
-		bool = False
-		
-	
+
 	num1 = num2
 	num2 = sum
+	
+print var
 
 
 
