df54456a51d3ad6f02cb60bb2e474bb5c71b935e
Jonathan Riordan <C13432152@mydit.ie>
2015-10-14 18:19:26 +0300

    Curl

--- Lab4/my_application/server.py
+++ Lab4/my_application/server.py
@@ -1,9 +1,23 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/hello")
 def hello():
     return "Hello World!"
 
+@app.route("/")
+def index():
+	return "Index File"
+
+@app.route("/user/paul")
+def paul():
+	return "User Paul"
+
+@app.route("/post/80")
+def post
+	return "Post 80"
+
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0', port=8080,debug=True)
