d86310284462b24aefdab3db5321b7cfb93f52b2
jriordan22849 <jriordan22849@gmail.com>
2015-12-02 17:18:08 +0200

    Lab11

--- lab11/consume-aws-message.py
+++ lab11/consume-aws-message.py
@@ -19,11 +19,11 @@ q_name = sys.argv[1]
 
 q = conn.get_queue(q_name)
 
-m = q.read()
+message = q.read()
 str = m.get_body()
 
 print("Message read: " + str)
 
-q.delete_message(m)
+q.delete_message(message)
 
 print("Message deleted from the queue")
